d46797b945cefae490db3da33c47428e6d9aee77
guimartinss <32970750+guimartinss@users.noreply.github.com>
2018-04-25 17:48:14 -0300

    Debug

--- calculadora.cpp
+++ calculadora.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 
 int incremento(int x){
-	return x++;
+    return ++x;
 }
 
 int decremento(int x){
-    return x--;
+    return --x;
 }
 
 int main(int argc, char** argv) {
-    cout << decremento(5);
+    cout << decremento(5) << endl;
+    cout << incremento(5) << endl;
+
 }
 
